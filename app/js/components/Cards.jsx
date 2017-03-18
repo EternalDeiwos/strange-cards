5a199a73564d2421657eb4b681a4950bb7dbc8a5
Greg Linklater <greglink49@gmail.com>
2017-03-18 22:02:27 +0200

    feat(Cards): improve interface

--- app/js/components/Cards.jsx
+++ app/js/components/Cards.jsx
@@ -7,13 +7,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import TextField from 'material-ui/TextField'
-import RaisedButton from 'material-ui/RaisedButton'
+import IconButton from 'material-ui/IconButton'
 import Toggle from 'material-ui/Toggle'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 
 import View from './common/View.jsx'
 import SkillCard from './common/SkillCard.jsx'
+import Icon from './common/Icon.jsx'
 
 import searchCardsAction from '../actions/searchCardsAction'
 import clearCardsAction from '../actions/clearCardsAction'
@@ -24,22 +26,26 @@ import downloadCardAction from '../actions/downloadCardAction'
  */
 const style = {
   rowStyle: {
+    position: 'relative',
     display: 'flex',
     flexDirection: 'row',
     flex: 100,
   },
   colStyle: {
+    position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     flex: 100,
   },
   buttonContainer: {
+    position: 'relative',
     display: 'flex',
     flexDirection: 'row',
     flex: 100,
     justifyContent: 'flex-end',
   },
   toggleContainerBase: {
+    position: 'relative',
     display: 'flex',
     flexDirection: 'row',
     flex: 100,
@@ -74,10 +80,21 @@ const style = {
     }
   },
   headingContainer: {
-    width: '540px',
+    position: 'relative',
+    width: '640px',
     display: 'flex',
     flex: 100,
     justifyContent: 'center'
+  },
+  downloadStyle: {
+    position: 'absolute',
+    top: '12px',
+    right: '15px',
+    zIndex: 10
+  },
+  centerIconStyle: {
+    justifyContent: 'center',
+    alignSelf: 'center'
   }
 }
 
@@ -116,6 +133,15 @@ class Cards extends React.Component {
   constructor (props) {
     super(props)
     this.formData = {}
+    this.state = { expanded: false }
+  }
+
+  componentDidMount() {
+    this.nameField.focus()
+  }
+
+  toggleExpanded () {
+    this.setState({ expanded: !this.state.expanded })
   }
 
   handleClear () {
@@ -154,10 +180,21 @@ class Cards extends React.Component {
 
   renderCards () {
     let { props: { result }} = this
-    let { headingContainer, rowStyle } = style
+    let { headingContainer, rowStyle, downloadStyle } = style
 
     if (result && result.length > 0) {
-      return result.map((item, index) => <SkillCard key={index} data={item} />)
+      return result.map((item, index) => {
+        let cardRef
+
+        return (
+          <div style={rowStyle} key={index}>
+            <SkillCard ref={card => cardRef = card} data={item} />
+            <IconButton tooltip="Download" style={downloadStyle} onTouchTap={ev => cardRef.save()}>
+              <Icon iconClassName="download" />
+            </IconButton>
+          </div>
+        )
+      })
     } else {
       return (
         <div style={rowStyle}>
@@ -170,71 +207,81 @@ class Cards extends React.Component {
   }
 
   render () {
-    let { rowStyle, colStyle, buttonContainer, toggleContainer, headingContainer } = style
+    let { rowStyle, colStyle, buttonContainer, toggleContainer, headingContainer, centerIconStyle } = style
 
     return (
       <div style={colStyle}>
-        <div style={rowStyle}>
-          <div style={headingContainer}>
-            <span>Search...</span>
-          </div>
-        </div>
-        <div style={rowStyle}>
-          <TextField
-            floatingLabelText="Name"
-            fullWidth={true}
-            onChange={(ev, value) => this.handleUpdate(value, 'name')}
-            id="name"
+        <Card expanded={this.state.expanded}>
+          <CardHeader
+            subtitle="Search..."
           />
-          <TextField
-            floatingLabelText="From"
-            fullWidth={true}
-            onChange={(ev, value) => this.handleUpdate(value, 'from')}
-            id="from"
-          />
-        </div>
-        <div style={rowStyle}>
-          <TextField
-            floatingLabelText="Source"
-            fullWidth={true}
-            onChange={(ev, value) => this.handleUpdate(value, 'source')}
-            id="source"
-          />
-          <SelectField
-            floatingLabelText="Type"
-            autoWidth={true}
-            onChange={(ev, key, payload) => this.handleUpdate(payload, 'type')}
-            selectionRenderer={value => value}
-            id="type">
-            {this.renderTypeMenuItems()}
-          </SelectField>
-        </div>
-        <div style={rowStyle}>
-          <TextField
-            floatingLabelText="Description"
-            fullWidth={true}
-            multiLine={true}
-            onChange={(ev, value) => this.handleUpdate(value, 'desc')}
-            id="desc"
-          />
-        </div>
-        <div style={rowStyle}>
-          <div style={toggleContainer('first')}>
-            <Toggle
-              label="Action"
-              onToggle={(ev, value) => this.handleUpdate(value, 'action')}
-            />
-          </div>
-          <div style={toggleContainer('last')}>
-            <Toggle
-              label="Purchasable"
-              onToggle={(ev, value) => this.handleUpdate(value, 'bought')}
-            />
-          </div>
-        </div>
-        <div style={buttonContainer}>
-          <RaisedButton label="Clear" primary={true} onTouchTap={(ev) => this.handleClear()} />
-        </div>
+          <CardText>
+            <div style={rowStyle}>
+              <TextField
+                floatingLabelText="Name"
+                fullWidth={true}
+                ref={field => this.nameField = field}
+                onChange={(ev, value) => this.handleUpdate(value, 'name')}
+                id="name"
+              />
+              <TextField
+                floatingLabelText="Class/Type/Descriptor"
+                fullWidth={true}
+                onChange={(ev, value) => this.handleUpdate(value, 'from')}
+                id="from"
+              />
+              <IconButton style={centerIconStyle} tooltip="Expand" onTouchTap={() => this.toggleExpanded()}>
+                <Icon iconClassName="angle-down" />
+              </IconButton>
+            </div>
+          </CardText>
+          <CardText expandable={true}>
+            <div style={rowStyle}>
+              <TextField
+                floatingLabelText="Source"
+                fullWidth={true}
+                onChange={(ev, value) => this.handleUpdate(value, 'source')}
+                id="source"
+              />
+              <SelectField
+                floatingLabelText="Type"
+                autoWidth={true}
+                onChange={(ev, key, payload) => this.handleUpdate(payload, 'type')}
+                selectionRenderer={value => value}
+                id="type">
+                {this.renderTypeMenuItems()}
+              </SelectField>
+            </div>
+            <div style={rowStyle}>
+              <TextField
+                floatingLabelText="Description"
+                fullWidth={true}
+                multiLine={true}
+                onChange={(ev, value) => this.handleUpdate(value, 'desc')}
+                id="desc"
+              />
+            </div>
+            <div style={rowStyle}>
+              <div style={toggleContainer('first')}>
+                <Toggle
+                  label="Action"
+                  onToggle={(ev, value) => this.handleUpdate(value, 'action')}
+                />
+              </div>
+              <div style={toggleContainer('last')}>
+                <Toggle
+                  label="Purchasable"
+                  onToggle={(ev, value) => this.handleUpdate(value, 'bought')}
+                />
+              </div>
+            </div>
+            <div style={buttonContainer}>
+              <IconButton tooltip="Clear" onTouchTap={ev => this.handleClear()}>
+                <Icon iconClassName="trash" />
+              </IconButton>
+            </div>
+          </CardText>
+        </Card>
         {this.renderCards()}
       </div>
     )
